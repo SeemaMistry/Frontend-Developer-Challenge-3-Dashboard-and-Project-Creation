6658d3a228bbb0acf251da68ff02440378975c20
SeemaMistry <seema.m.1996@gmail.com>
2023-11-26 23:20:28 -0500

    chore: remove hardcoded height=18rem

--- prepr-dashboard/src/components/Card.js
+++ prepr-dashboard/src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Card = ({children, height}) => {
-    height = '18rem'
+    
   return (
     <div className='bg-white rounded shadow p-3 m-3' style={{height: `${height}`}}>
         {children}
