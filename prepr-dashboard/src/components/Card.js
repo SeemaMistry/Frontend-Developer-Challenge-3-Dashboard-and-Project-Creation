316992985c3c14179a4b42b51d59f9ec20630a4e
SeemaMistry <seema.m.1996@gmail.com>
2023-11-26 23:11:36 -0500

    feat: pass card height as prop to each card-row

--- prepr-dashboard/src/components/Card.js
+++ prepr-dashboard/src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Card = ({children}) => {
+const Card = ({children, height}) => {
+    height = '18rem'
   return (
-    <div className='bg-white rounded shadow p-3 m-3'>
+    <div className='bg-white rounded shadow p-3 m-3' style={{height: `${height}`}}>
         {children}
     </div>
   )
