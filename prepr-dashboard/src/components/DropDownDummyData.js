e9fbb52d1f6886c7963aa9c14ba044198e007b51
SeemaMistry <seema.m.1996@gmail.com>
2023-11-28 15:34:12 -0500

    feat: export all dropdown menu values as objects

---
+++ prepr-dashboard/src/components/DropDownDummyData.js
@@ -0,0 +1,25 @@
+// export const languageList = {
+//     label: '', 
+//     defaultSelected: '',
+//     options:['English', 'French', 'Spanish', 'German']
+// }
+export const organizationList = {
+    label: 'Organization', 
+    defaultSelected: 'All Organizations',
+    options: ['Prepr', 'Youth Boost']
+}
+export const labsList = {
+    label: 'Lab', 
+    defaultSelected: 'All Labs',
+    options: ['Getting Started Using the PreprLabs Network', 'Career Planning', 'Web Developer']
+}
+export const challengesList = {
+    label: 'Challenge', 
+    defaultSelected: 'All Challenges',
+    options: ['Creating a Project', 'Complete Your Profile', 'Exploring Labs and Challenges']
+}
+export const sortByList = {
+    label: 'Sort by', 
+    defaultSelected: 'Learning Points',
+    options: ['Learning Rank', 'Achievements']
+}
