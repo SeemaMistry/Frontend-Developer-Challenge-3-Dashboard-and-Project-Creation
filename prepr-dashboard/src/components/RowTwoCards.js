46b2b9885ac2c561f3940928872ca993b8bd05bd
SeemaMistry <seema.m.1996@gmail.com>
2023-11-27 23:45:56 -0500

    chore: clean up code

--- prepr-dashboard/src/components/RowTwoCards.js
+++ prepr-dashboard/src/components/RowTwoCards.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Card from './Card'
 import CardTitle from '../components/CardTitle'
+import Description from './Description'
 import RedirectButton from './RedirectButton'
+import clipboardImg from '../assets/latest_achievement.png'
+import iconYellowBadge from '../assets/iconYellowBadge.png'
 
 const RowTwoCards = ({height}) => {
+  const centerImage = 'mx-auto d-block'
   return (
     <div className='row'>
     <div className='col-md-8'>
@@ -14,8 +18,17 @@ const RowTwoCards = ({height}) => {
     </div>
     <div className='col'>
       <Card height={height}>
-        <CardTitle title='Card title will be here'/>
-        <RedirectButton label='Explore more'/>
+        <div className='d-flex justify-content-start'>
+          <img src={iconYellowBadge}/>
+          <div className='my-auto'>
+            <CardTitle title='Latest Achievement '/>
+          </div>
+        </div>
+        <Description description='Complete challenges to gain badges and achievements. Get started now!'/>
+        <img className={`${centerImage}`} src={clipboardImg} />
+        <div className='d-flex justify-content-end' style={{marginTop: '10%'}}>
+          <RedirectButton label='Explore more'/>
+        </div>
       </Card>
     </div>
   </div> 
