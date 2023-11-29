847ff831a234c12dc4bed2b4f1cfb5061095cf1f
SeemaMistry <seema.m.1996@gmail.com>
2023-11-29 01:06:36 -0500

    feat: pass store to App via provider tag

--- prepr-dashboard/src/App.js
+++ prepr-dashboard/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import './App.css';
 
 import Layout from './hocs/Layout'
@@ -9,14 +11,16 @@ import CreateProject from './containers/CreateProject'
 
 const App = () => {
   return (
-    <Router>
-    <Layout>
-      <Routes>
-        <Route exact path='/dashboard' Component={Dashboard}></Route>
-        <Route exact path='/create/project' Component={CreateProject}></Route>
-      </Routes>
-    </Layout>
-  </Router>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path='/dashboard' Component={Dashboard}></Route>
+            <Route exact path='/create/project' Component={CreateProject}></Route>
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
   );
 }
 
